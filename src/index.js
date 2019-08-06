ab831070748afa1533eaea7cd61438c22efde0be
hussein-salloum <Hus8sein.Sal7loum@gmail.com>
2019-08-06 18:15:35 +0300

    react router setup with couple of pages in app

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// import router
+import { BrowserRouter } from "react-router-dom";
+
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+
+    , document.getElementById('root')
+);
 
 serviceWorker.unregister();
